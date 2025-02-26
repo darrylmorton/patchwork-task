b07627477d99343486d151c7b3d718f82b204253
darrylmorton <darryl.j.morton@gmail.com>
2025-02-26 14:20:15 +0000

    project files with test setup

---
+++ README.md
@@ -0,0 +1,11 @@
+# Patchwork Task
+
+## Requirements
+node.js v22.14+  
+npm v10.9+ 
+
+## Setup
+
+## Start
+
+## Test
---
+++ test/unit/user.test.ts
@@ -0,0 +1,17 @@
+import { expect } from 'chai'
+
+describe('Library Users', () => {
+  it.only('As a library user, I would like to be able to find books by my favourite author, so that I know if they are available in the library', () => {
+    expect(false).to.equal(true)
+  })
+
+  it('As a library user, I would like to be able to find books by title, so that I know if they are available in the library', () => {})
+
+  it('As a library user, I would like to be able to find books by ISBN, so that I know if they are available in the library', () => {})
+
+  it('As a library user, I would like to be able to borrow a book, so I can read it at home', () => {})
+
+  it('As the library owner, I would like to know how many books are being borrowed, so I can see how many are outstanding', () => {})
+
+  it('As a library user, I should be to prevented from borrowing reference books, so that they are always available', () => {})
+})
